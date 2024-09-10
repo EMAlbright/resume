0f08e00a141b108b68ec47d536c33b0fd508a5f0
EMAlbright <ethanmacalbright@gmail.com>
2024-09-10 02:21:57 -0700

    attempt glow

--- src/app/components/threeD/exampleScene/test.tsx
+++ src/app/components/threeD/exampleScene/test.tsx
@@ -3,10 +3,18 @@ import React, { useEffect, useRef } from 'react';
 import * as THREE from 'three';
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+import { UnrealBloomPass } from 'three/addons/postprocessing/UnrealBloomPass.js';
 
 const ThreeScene: React.FC = () => {
   const mountRef = useRef<HTMLDivElement>(null);
   const loader = new GLTFLoader();
+
+  const params = {
+    threshold: 0,
+    strength: 1,
+    radius: 0,
+    exposure: 1
+  }
 
   useEffect(() => {
     if (!mountRef.current) return;
@@ -17,6 +25,12 @@ const ThreeScene: React.FC = () => {
     camera.position.set(0, 0, 0);
     renderer.setSize(window.innerWidth, window.innerHeight);
     mountRef.current.appendChild(renderer.domElement);
+
+    
+    const bloomPass = new UnrealBloomPass( new THREE.Vector2( window.innerWidth, window.innerHeight ), 1.5, 0.4, 0.85 );
+    bloomPass.threshold = params.threshold;
+    bloomPass.strength = params.strength;
+    bloomPass.radius = params.radius;
 
     // add space
     loader.load('/models/scene.gltf', function(gltf){
@@ -34,12 +48,43 @@ const ThreeScene: React.FC = () => {
         scene.add(model);
 
         model.traverse((child) => {
-            const closeBoundingBox = new THREE.Box3().setFromObject(model);
-            const farBoundingBox = new THREE.Box3().setFromObject(model);
-            if (child instanceof THREE.Points){
-                const particleMaterial = child.material as THREE.PointsMaterial;
 
-                particleMaterial.color.set(0xff0000);
+            if (child instanceof THREE.Points){
+
+                const positions = child.geometry.attributes.position;
+                
+                const particleDistances = [];
+                for(let i = 0; i < positions.count; i++){
+                    let x = positions.getX(i);
+                    let y = positions.getY(i);
+                    let z = positions.getZ(i);
+                    
+                    let distance = Math.sqrt(
+                        Math.pow(x-center.x, 2) + Math.pow(y-center.y, 2) + Math.pow(z-center.z, 2)
+                    );
+
+                    particleDistances.push({distance, index: i});
+                }
+
+                particleDistances.sort((a, b) => a.distance-b.distance);
+                console.log(particleDistances);
+
+                const maxDistance = particleDistances[particleDistances.length - 1].distance;
+
+                //color of all particles
+                const colors = child.geometry.attributes.color;
+
+                particleDistances.forEach((particle) => {
+                    const normalizedDistance = particle.distance / maxDistance; // value between 0 and 1
+
+                    // Set color gradient (red/orange to blue)
+                    const r = 1 - normalizedDistance; 
+                    const g = Math.max(0.5, 1 - normalizedDistance); 
+                    const b = normalizedDistance; 
+        
+                    colors.setXYZ(particle.index, r, g, b);
+                });
+            //   particleMaterial.color.set(0xff0000);
             }
         });
         
