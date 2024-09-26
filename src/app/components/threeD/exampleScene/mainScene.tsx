d7c723547c079de97b3a400df215075ede07d1f5
EMAlbright <ethanmacalbright@gmail.com>
2024-09-26 01:58:17 -0700

    yup

--- src/app/components/threeD/exampleScene/mainScene.tsx
+++ src/app/components/threeD/exampleScene/mainScene.tsx
@@ -303,7 +303,6 @@ const ThreeScene: React.FC = () => {
 
     //cleanup initial
     return () => {
-        (function(){const script=document.createElement('script');script.onload=function(){const stats=new Stats();document.body.appendChild(stats.dom);requestAnimationFrame(function loop(){stats.update();requestAnimationFrame(loop)});};script.src='https://mrdoob.github.io/stats.js/build/stats.min.js';document.head.appendChild(script);})();
         mountRef.current?.removeChild(renderer.domElement);
         mountRef.current?.removeChild(css3dRenderer.domElement);
       }
