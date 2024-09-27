e990ffb2df25692776f46bb7cd2b904be94ca67b
EMAlbright <ethanmacalbright@gmail.com>
2024-09-27 14:51:00 -0700

    a single letter

--- src/app/components/twoD/panelContent/panelContent.tsx
+++ src/app/components/twoD/panelContent/panelContent.tsx
@@ -89,7 +89,7 @@ export const CreateAboutText = () => (
         height="400" 
         controls
     >
-        <source src="/demoVideos/stockmarket.mp4" type="video/mp4" />
+        <source src="/demoVideos/stockMarket.mp4" type="video/mp4" />
         Your browser does not support the video tag.
     </video>
     </div>
